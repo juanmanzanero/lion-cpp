88f4296108d6a921f454274bcc199e2ca63b5cce
Juan Manzanero <juan.manzanero@upm.es>
2021-11-16 08:33:12 +0100

    Update gtest.cmake

--- cmake/third-party/gtest.cmake
+++ cmake/third-party/gtest.cmake
@@ -2,7 +2,7 @@ if (${BUILD_GTEST})
 include(ExternalProject)
 ExternalProject_Add(googletest
     GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG master
+    GIT_TAG main
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${THIRD_PARTY_DIR}/thirdparty -DCMAKE_BUILD_TYPE=Release -DBUILD_GMOCK=OFF
     PREFIX ${THIRD_PARTY_DIR}/gtest
     SOURCE_DIR "${THIRD_PARTY_DIR}/gtest/source"
