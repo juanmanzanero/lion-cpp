84103ac05970db2b3e008e6c922c69983df55a6b
Diego Lodares <diego.lodares@airbus.com>
2023-03-25 16:02:52 +0100

    [DLG] Update constants.h

--- lion/foundation/constants.h
+++ lion/foundation/constants.h
@@ -6,7 +6,7 @@
 enum Axis{ X, Y, Z };
 
 template<typename T = double>
-constexpr T pi_T = static_cast<T>(3.1415926535897932385L); // const T pi = static_cast<T>(std::acos(static_cast<T>(-1.)));
+constexpr T pi_T = T{ 3.1415926535897932385L };
 
 template<typename T = double>
 constexpr T eps_T = std::numeric_limits<T>::epsilon();
@@ -15,34 +15,22 @@ template<typename T = double>
 constexpr T inf_T = std::numeric_limits<T>::infinity();
 
 template<typename T = double>
-struct nan_T
-{
-    static inline T nan = static_cast<T>(std::nan(""));
-};
+constexpr T nan_T = std::numeric_limits<T>::quiet_NaN();
 
-template<>
-struct nan_T<float>
-{
-    static inline float nan = std::nanf("");
-};
-
-template<>
-struct nan_T<long double>
-{
-    static inline double nan = std::nanl("");
-};
 
 // handy names when using double, our usual case
-constexpr double pi{ pi_T<double> };
-constexpr double eps{ eps_T<double> };
-constexpr double inf{ inf_T<double> };
-constexpr double Inf{ inf };
-inline static double NaN{ nan_T<double>::nan };
+constexpr auto pi{ pi_T<double> };
+constexpr auto eps{ eps_T<double> };
+constexpr auto inf{ inf_T<double> };
+constexpr auto Inf{ inf };
+constexpr auto nan{ nan_T<double> };
+constexpr auto NaN{ nan };
 
-constexpr inline double g0{9.81};
-constexpr inline double KMH{1.0/3.6};
-constexpr inline double RPM{pi/30.0};
-constexpr inline double CV{735.499};
-constexpr inline double DEG{pi/180.0};
-constexpr inline double RAD{180.0/pi};
+constexpr double g0{ 9.81 };
+constexpr double KMH{ 1.0 / 3.6 };
+constexpr double RPM{ pi / 30.0 };
+constexpr double CV{ 735.499 };
+constexpr double DEG{ pi / 180.0 };
+constexpr double RAD{ 180.0 / pi };
+
 #endif
